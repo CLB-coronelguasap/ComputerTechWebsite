73c92e12f923c669ba64685fe0ba9f8daae519d0
CLB-coronelguasap <caleb.serranoobando@education.nsw.gov.au>
2025-05-07 19:51:00 +1000

    test security

--- assets/js/security.js
+++ assets/js/security.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     // type one text in the typwriter
     // keeps calling itself until the text is finished
     function typeWriter(text, i, fnCallback) {
-      // chekc if text isn't finished yet
+      // check if text isn't finished yet
       if (i < (text.length)) {
         // add next character to h1
        document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
@@ -22,20 +22,17 @@ document.addEventListener('DOMContentLoaded',function(event){
       }
     }
     // start a typewriter animation for a text in the dataText array
-     function StartTextAnimation(i) {
-       if (typeof dataText[i] == 'undefined'){
-          setTimeout(function() {
-            StartTextAnimation(0);
-          }, 20000);
-       }
-       // check if dataText[i] exists
-      if (i < dataText[i].length) {
-        // text exists! start typewriter animation
-       typeWriter(dataText[i], 0, function(){
-         // after callback (and whole text has been animated), start next text
-         StartTextAnimation(i + 1);
-       });
-      }
+    function StartTextAnimation(i) {
+        if (typeof dataText[i] == 'undefined') {
+            setTimeout(function() {
+                StartTextAnimation(0); // Restart from the beginning
+            }, 20000);
+        } else if (i < dataText.length) { // Correct condition
+            typeWriter(dataText[i], 0, function() {
+                // After callback (and whole text has been animated), start next text
+                StartTextAnimation(i + 1);
+            });
+        }
     }
     // start the text animation
     StartTextAnimation(0);
