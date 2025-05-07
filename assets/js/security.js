7890e28a52f0516e60323137907545e0523b4509
CLB-coronelguasap <caleb.serranoobando@education.nsw.gov.au>
2025-05-07 19:39:02 +1000

    f

--- assets/js/security.js
+++ assets/js/security.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
     // array with texts to type in typewriter
-    var dataText = [ "#!", "SHEBANG", "CYBERSECURITY", "INNOVATION"];
+    var dataText = [ "CRYPTOMINERS", "PHISHING", "RANSOMWARE", "TROJANS", "SPYWARE", "WORM", "ADWARE", "ROOTKIT", "KEYLOGGER", "EXPLOIT", "VIRUS", "MALWARE", "BOTNET", "DDoS", "SQL INJECTION", "XSS", "RAT" ];
     
     // type one text in the typwriter
     // keeps calling itself until the text is finished
