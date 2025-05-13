da2b853a1d3530eef26013064967c08ea2c1f448
CLB-coronelguasap <caleb.serranoobando@education.nsw.gov.au>
2025-05-13 17:50:55 +1000

    more modal tweaks

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,8 +23,6 @@ document.addEventListener('DOMContentLoaded',function(event){
         // Get the modal
     var modal = document.getElementById("music-modal");
 
-    var hero = document.getElementsBy-ClassName("hero")[0]
-
     var audio = document.getElementById("audio")
     // Get the button that opens the modal
     var btn = document.getElementById("music-btn");
@@ -38,14 +36,16 @@ document.addEventListener('DOMContentLoaded',function(event){
 
     // When the user clicks the button, open the modal 
     btn.onclick = function() {
+      modal.classList.add("fadein");
       modal.style.display = "block";
-      hero.style.display = "none"
     }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-      modal.style.display = "none";
-      hero.style.display = "block"
+      setTimeout(() => {
+        modal.style.display = "none";
+        modal.classList.remove("fadeout");
+      }, 400);
     }
 
 
@@ -53,10 +53,14 @@ document.addEventListener('DOMContentLoaded',function(event){
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
       if (event.target == modal) {
-        modal.style.display = "none";
-        hero.style.display = "block"
+        setTimeout(() => {
+          modal.style.display = "none";
+          modal.classList.remove("fadeout");
+        }, 400);
       }
     }
+
+
 
     enable.onclick = function() {
       audio.play()
