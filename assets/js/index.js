f534002f9c2263fb94222f0e94eded8241e5d020
CLB-coronelguasap <caleb.serranoobando@education.nsw.gov.au>
2025-05-13 14:15:13 +1000

    hava

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,6 +19,33 @@ document.addEventListener('DOMContentLoaded',function(event){
     } else {
       console.log("GONK WILL NOT APPEAR!")
     }
+
+        // Get the modal
+    var modal = document.getElementById("music-modal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
     // type one text in the typwriter
     // keeps calling itself until the text is finished
     function typeWriter(text, i, fnCallback) {
