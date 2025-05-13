80baa50000209eb6fbb239b0ff067b0039e0f4ce
CLB-coronelguasap <caleb.serranoobando@education.nsw.gov.au>
2025-05-13 15:03:01 +1000

    nagila

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,8 +23,15 @@ document.addEventListener('DOMContentLoaded',function(event){
         // Get the modal
     var modal = document.getElementById("music-modal");
 
+    var hero = document.getElementsByClassName("hero-content")
+
+    var audio = document.getElementById("audio")
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    var btn = document.getElementById("music-btn");
+
+    var enable = document.getElementById("enable-btn")
+
+    var disable = document.getElementById("disable-btn")
 
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
@@ -32,18 +39,31 @@ document.addEventListener('DOMContentLoaded',function(event){
     // When the user clicks the button, open the modal 
     btn.onclick = function() {
       modal.style.display = "block";
+      hero.style.display = "none"
     }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       modal.style.display = "none";
+      hero.style.display = "block"
     }
+
+
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
       if (event.target == modal) {
         modal.style.display = "none";
+        hero.style.display = "block"
       }
+    }
+
+    enable.onclick = function() {
+      audio.play()
+    }
+
+    disable.onclick = function() {
+      audio.pause()
     }
 
     // type one text in the typwriter
